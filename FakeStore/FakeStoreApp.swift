fe94a67432430b76d7054370e0b0feb4816c3b1b
Carl-Johan Heinze <cj.heinze@parkster.com>
2023-02-19 14:22:51 +0100

    Initial Commit

---
+++ FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by Carl-Johan Heinze on 2023-02-19.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
